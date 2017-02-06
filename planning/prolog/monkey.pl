7832b67b58101a4fd6a0bf75665fba8b458b268b
Stathis Sideris <stathis@pixelated-noise.com>
2017-02-06 04:02:53 +0200

    Prolog+STRIPS monkey problem solution

--- planning/prolog/monkey.pl
+++ planning/prolog/monkey.pl
@@ -1,61 +1,77 @@
-%% opn( go(Agent, From, To),
-%%      [at(Agent, From)],
-%%      [at(Agent, From)],
-%%      [at(Agent, To)]).
+opn( go(Agent, From, To),
+     [at(Agent, From)],
+     [at(Agent, From)],
+     [at(Agent, To)]).
 
 opn( push(B, X, Y),
-     [at(B, X),
-      on(B, floor),
-      at(monkey, X),
-      on(monkey, floor)],
+     [at(monkey, X),
+      at(B, X),
+      on(monkey, floor),
+      on(B, floor)],
      [at(monkey, X),
       at(B, X)],
      [at(monkey, Y),
       at(B, Y)]).
 
-%% opn( climb_on(B),
-%%      [at(monkey, X),
-%%       at(B, X),
-%%       on(monkey, floor),
-%%       on(B, floor)],
-%%      [on(monkey, floor)],
-%%      [on(monkey, B)]).
+opn( climb_on(B),
+     [at(monkey, X),
+      at(B, X),
+      on(monkey, floor),
+      on(B, floor)],
+     [on(monkey, floor)],
+     [on(monkey, B)]).
 
-%% opn( grab(B),
-%%      [on(monkey, box),
-%%       at(box, X),
-%%       at(B, X),
-%%       status(B, hanging)],
-%%      [status(B, hanging)],
-%%      [status(B, grabbed)]).
+opn( grab(B),
+     [on(monkey, box),
+      at(box, X),
+      at(B, X),
+      status(B, hanging)],
+     [status(B, hanging)],
+     [status(B, grabbed)]).
 
 notmember(X, S):-
-    not(memberchk(X, S)).
+    not(member(X, S)).
+
+%% list:subset does not backtrack
+%% because it's based on http://www.swi-prolog.org/pldoc/man?predicate=memberchk/2
+is_subset([], _) :- !.
+is_subset([E|R], Set) :-
+    member(E, Set),
+    is_subset(R, Set).
 
 precons_present(P, S):-
-    subset(P, S).
+    is_subset(P, S).
 
 solve(State, Goal, Plan, Plan):-
     subset(Goal, State).
 
 solve(State, Goal, Sofar, Plan):-
     opn(Op, Precons, Delete, Add),
+    %write(Op), nl,
     notmember(Op, Sofar),
     precons_present(Precons, State),
+    is_set(Precons),
     subtract(State, Delete, Remainder),
     append(Add, Remainder, NewState),
     solve(NewState, Goal, [Op|Sofar], Plan).
 
 test(Goal, Plan):-
-    solve([on(monkey, floor),
+    solve([at(monkey, loc_a),
+           on(monkey, floor),
+
+           at(box, loc_b),
            on(box, floor),
-           at(monkey, loc_a),
-           at(box, loc_a)
-           %at(bananas, loc_b),
-           %status(bananas, hanging)
+
+           at(bananas, loc_c),
+           status(bananas, hanging)
           ],
           Goal,
           [],
           Plan).
 
-%?- test([status(bananas, grabbed)], Plan).
+% To run:
+% > cd [THIS-DIR]
+% > /usr/local/Cellar/swi-prolog/7.2.3_2/bin/swipl
+% [monkey].
+% test([status(bananas, grabbed)], Plan).
+% Plan = [grab(bananas), climb_on(box), push(box, loc_b, loc_c), go(monkey, loc_a, loc_b)] .
